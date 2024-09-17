1e13d773fe127e38911f780cf666cc447b62555e
prabhjot98 <pssandhu@ucdavis.edu>
2024-09-17 14:11:04 -0700

    feat: save and load games
    also added game version control and some slight refactoring for consts

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"os"
@@ -14,41 +15,48 @@ import (
 	"github.com/rivo/uniseg"
 )
 
+const SAVE_NAME = "save.json"
+const DAILY_TOKENS = 20
+const LOSE_JACKPOT_INCREASE = 5
+const SPIN_TICKS = 90
+const GAME_VERSION = "0.1"
+
 type model struct {
-	tokens        int
-	dollars       int
-	ticksLeft     int
-	spinnerMsg    string
-	spinning      bool
-	isDay         bool
-	windowWidth   int
-	windowHeight  int
-	jackpotAmount int
-	slot          Slot
-	isFreeSpin    bool
-	feverMode     bool
-	symbolDT      SymbolDropTable
-	multiplier    int
+	Tokens          int             `json:"tokens"`
+	Dollars         int             `json:"dollars"`
+	TicksLeft       int             `json:"ticksLeft"`
+	SpinnerMsg      string          `json:"spinnerMsg"`
+	Spinning        bool            `json:"spinning"`
+	IsDay           bool            `json:"isDay"`
+	JackpotAmount   int             `json:"jackpotAmount"`
+	Slot            Slot            `json:"slot"`
+	IsFreeSpin      bool            `json:"isFreeSpin"`
+	FeverMode       bool            `json:"feverMode"`
+	SymbolDropTable SymbolDropTable `json:"symbolDT"`
+	Multiplier      int             `json:"multiplier"`
+	LastPlayed      time.Time       `json:"lastPlayed"`
+	GameVersion     string          `json:"gameVesion"`
+	windowWidth     int
+	windowHeight    int
 }
 
 type SymbolDropTable map[emoji.Emoji]int
-
 type TickMsg time.Time
 type Symbol emoji.Emoji
 type Slot [3]Symbol
 
 func initTable() SymbolDropTable {
 	return map[emoji.Emoji]int{
-		emoji.Keycap1:       100,
-		emoji.Keycap2:       100,
-		emoji.Keycap3:       100,
-		emoji.Keycap4:       100,
+		emoji.Keycap1:       0,
+		emoji.Keycap2:       0,
+		emoji.Keycap3:       0,
+		emoji.Keycap4:       0,
 		emoji.Keycap5:       100,
 		emoji.Keycap6:       0,
-		emoji.Keycap7:       0,
+		emoji.Keycap7:       100,
 		emoji.Keycap8:       0,
 		emoji.Keycap9:       0,
-		emoji.Keycap10:      0,
+		emoji.Keycap10:      100,
 		emoji.HoneyPot:      50,
 		emoji.Joker:         50,
 		emoji.HundredPoints: 50,
@@ -62,16 +70,22 @@ func initTable() SymbolDropTable {
 	}
 }
 
+// roll a number between [min,max)
+func roll(min, max int) int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	roll := r.Intn(max-min) + min
+	return roll
+}
+
 func (s SymbolDropTable) rollTable() Symbol {
 	maxRoll := 0
 	for _, weight := range s {
 		maxRoll += weight
 	}
 
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	roll := r.Intn(maxRoll) // generate a random int between [0 and maxRoll)
-
+	roll := roll(0, maxRoll)
 	cumulativeProbability := 0
+
 	for e, prob := range s {
 		cumulativeProbability += prob
 		if roll < cumulativeProbability {
@@ -84,30 +98,20 @@ func (s SymbolDropTable) rollTable() Symbol {
 }
 
 func (m *model) turnToDay() {
-	m.isDay = true
-	m.symbolDT[emoji.Sunrise] = 0
-	m.symbolDT[emoji.FullMoon] = initTable()[emoji.FullMoon]
-	m.symbolDT[emoji.Joker] = initTable()[emoji.Joker]
-	m.symbolDT[emoji.Skull] = initTable()[emoji.Skull]
-	m.symbolDT[emoji.Keycap6] = 0
-	m.symbolDT[emoji.Keycap7] = 0
-	m.symbolDT[emoji.Keycap8] = 0
-	m.symbolDT[emoji.Keycap9] = 0
-	m.symbolDT[emoji.Keycap10] = 0
+	m.IsDay = true
+	m.SymbolDropTable[emoji.Sunrise] = 0
+	m.SymbolDropTable[emoji.FullMoon] = initTable()[emoji.FullMoon]
+	m.SymbolDropTable[emoji.Joker] = initTable()[emoji.Joker]
+	m.SymbolDropTable[emoji.Skull] = initTable()[emoji.Skull]
 
 }
 
 func (m *model) turnToNight() {
-	m.isDay = false
-	m.symbolDT[emoji.Sunrise] = 50
-	m.symbolDT[emoji.FullMoon] = 0
-	m.symbolDT[emoji.Joker] = initTable()[emoji.Joker] * 4
-	m.symbolDT[emoji.Skull] = initTable()[emoji.Skull] * 2
-	m.symbolDT[emoji.Keycap6] = 100
-	m.symbolDT[emoji.Keycap7] = 100
-	m.symbolDT[emoji.Keycap8] = 100
-	m.symbolDT[emoji.Keycap9] = 100
-	m.symbolDT[emoji.Keycap10] = 100
+	m.IsDay = false
+	m.SymbolDropTable[emoji.Sunrise] = initTable()[emoji.FullMoon]
+	m.SymbolDropTable[emoji.FullMoon] = 0
+	m.SymbolDropTable[emoji.Joker] = initTable()[emoji.Joker] * 4
+	m.SymbolDropTable[emoji.Skull] = initTable()[emoji.Skull] * 2
 }
 
 func (s Symbol) toEmoji() emoji.Emoji {
@@ -220,51 +224,49 @@ func initSlot() Slot {
 }
 
 func (m *model) SpinSlots() {
-	if m.ticksLeft > 60 {
-		m.slot[0] = m.symbolDT.rollTable()
+	if m.TicksLeft > 60 {
+		m.Slot[0] = m.SymbolDropTable.rollTable()
 	}
-	if m.ticksLeft > 30 {
-		m.slot[1] = m.symbolDT.rollTable()
-
+	if m.TicksLeft > 30 {
+		m.Slot[1] = m.SymbolDropTable.rollTable()
 	}
-	m.slot[2] = m.symbolDT.rollTable()
+	m.Slot[2] = m.SymbolDropTable.rollTable()
 }
 
 func (m *model) handleWin() {
-	if m.slot[0] == Symbol(emoji.Joker) &&
-		m.slot[1] == Symbol(emoji.Joker) &&
-		m.slot[2] == Symbol(emoji.Joker) {
-		m.spinnerMsg = "Hahahahahahahaha " + emoji.RollingOnTheFloorLaughing.String() + emoji.RollingOnTheFloorLaughing.String() + emoji.RollingOnTheFloorLaughing.String()
+	if m.Slot[0] == Symbol(emoji.Joker) &&
+		m.Slot[1] == Symbol(emoji.Joker) &&
+		m.Slot[2] == Symbol(emoji.Joker) {
+		m.SpinnerMsg = "Hahahahahahahaha " + emoji.RollingOnTheFloorLaughing.String() + emoji.RollingOnTheFloorLaughing.String() + emoji.RollingOnTheFloorLaughing.String()
 		return
 	}
 
-	switch m.slot.getFirstNonJokerSymbol() {
+	switch m.Slot.getFirstNonJokerSymbol() {
 	case Symbol(emoji.HoneyPot):
-		m.dollars += m.jackpotAmount
-		m.spinnerMsg = "🎉🎉🎉YOU HIT THE JACKPOT!!! YOU WON " + strconv.Itoa(
-			m.jackpotAmount,
+		m.Dollars += m.JackpotAmount
+		m.SpinnerMsg = "🎉🎉🎉YOU HIT THE JACKPOT!!! YOU WON " + strconv.Itoa(
+			m.JackpotAmount,
 		) + " dollars!!!!🎉🎉🎉"
-		m.jackpotAmount = 0
+		m.JackpotAmount = 0
 	case Symbol(emoji.HundredPoints):
-		m.dollars += 100
-		m.spinnerMsg = "🎉 Nice 100! You won 100 dollars!"
+		m.Dollars += 100
+		m.SpinnerMsg = "🎉 Nice 100! You won 100 dollars!"
 	case Symbol(emoji.GameDie):
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		roll := r.Intn(6) + 1 // generate a random int between [1 and 6]
-		m.multiplier = roll
-		m.spinnerMsg = "Your new multiplier is x" + strconv.Itoa(roll)
+		roll := roll(1, 6)
+		m.Multiplier = roll
+		m.SpinnerMsg = "Your new multiplier is x" + strconv.Itoa(roll)
 	case Symbol(emoji.FreeButton):
-		m.isFreeSpin = true
-		m.spinnerMsg = "Nice! You got a free spin! Go ahead and reroll!"
+		m.IsFreeSpin = true
+		m.SpinnerMsg = "Nice! You got a free spin! Go ahead and reroll!"
 	case Symbol(emoji.Sunrise):
 		m.turnToDay()
-		m.spinnerMsg = "It's back to being daytime! Numbers 1-5 can show up and the chances for jokers and skulls is back to normal."
+		m.SpinnerMsg = "It's back to being daytime! The chances for jokers and skulls is back to normal."
 	case Symbol(emoji.FullMoon):
 		m.turnToNight()
-		m.spinnerMsg = "It's nighttime now! Numbers 1-10 can show up and the chance of jokers and skulls is increased!"
+		m.SpinnerMsg = "It's nighttime now! The chance of jokers and skulls is increased!"
 	case Symbol(emoji.Fire):
-		m.feverMode = true
-		m.spinnerMsg = "You're in fever mode now!"
+		m.FeverMode = true
+		m.SpinnerMsg = "You're in fever mode now!"
 	case Symbol(emoji.Keycap1):
 		fallthrough
 	case Symbol(emoji.Keycap2):
@@ -286,33 +288,44 @@ func (m *model) handleWin() {
 	case Symbol(emoji.Keycap10):
 		feverMulti := 1
 		feverTxt := ""
-		if m.feverMode {
-			m.feverMode = false
+		if m.FeverMode {
+			m.FeverMode = false
 			feverMulti = 2
 			feverTxt = "\nBut doubled because you were in fever mode!" + emoji.Fire.String()
 
 		}
-		wonDollars := m.slot.getFirstNonJokerSymbol().toInt() * m.multiplier * feverMulti
-		m.dollars += wonDollars
-		m.spinnerMsg = "🎉You won " + strconv.Itoa(wonDollars) + " dollars!🎉" + feverTxt
+		wonDollars := m.Slot.getFirstNonJokerSymbol().toInt() * m.Multiplier * feverMulti
+		m.Dollars += wonDollars
+		m.SpinnerMsg = "🎉You won " + strconv.Itoa(wonDollars) + " dollars!🎉" + feverTxt
+	}
+}
+
+func (m model) saveGame() {
+	data, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+	}
+	err = os.WriteFile(SAVE_NAME, data, 0644)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
 	}
 }
 
 func (m *model) finishSpin() {
-	m.spinning = false
-	if m.slot.allSymbolsMatch() || m.slot.allNumbersMatch() != -1 {
+	m.Spinning = false
+	if m.Slot.allSymbolsMatch() || m.Slot.allNumbersMatch() != -1 {
 		m.handleWin()
 	} else {
-		m.spinnerMsg = "Try again :("
-		m.jackpotAmount += 10
+		m.SpinnerMsg = "Try again :("
+		m.JackpotAmount += LOSE_JACKPOT_INCREASE
 	}
-
+	m.saveGame()
 }
 
 func (m *model) doTick() tea.Cmd {
 	m.SpinSlots()
-	m.ticksLeft--
-	if m.ticksLeft == 0 {
+	m.TicksLeft--
+	if m.TicksLeft == 0 {
 		m.finishSpin()
 		return nil
 	}
@@ -321,25 +334,69 @@ func (m *model) doTick() tea.Cmd {
 	})
 }
 
-func initialModel() model {
-	// TODO get the initial model from local storage
+func (m *model) updateLastPlayed() {
+	m.LastPlayed = time.Now()
+}
+
+func initialGame() model {
 	return model{
-		tokens:        100,
-		dollars:       0,
-		ticksLeft:     0,
-		spinning:      false,
-		spinnerMsg:    "",
-		isDay:         true,
-		jackpotAmount: 0,
-		multiplier:    1,
-		isFreeSpin:    false,
-		feverMode:     false,
-		slot:          initSlot(),
-		symbolDT:      initTable(),
+		Tokens:          100,
+		Dollars:         0,
+		TicksLeft:       0,
+		Spinning:        false,
+		SpinnerMsg:      "Spin to win!",
+		IsDay:           true,
+		JackpotAmount:   0,
+		Multiplier:      1,
+		IsFreeSpin:      false,
+		FeverMode:       false,
+		Slot:            initSlot(),
+		SymbolDropTable: initTable(),
+		LastPlayed:      time.Now(),
+		GameVersion:     GAME_VERSION,
 	}
 }
 
-func (m model) Init() tea.Cmd { return nil }
+func loadGame() model {
+	jsonData, err := os.ReadFile(SAVE_NAME)
+	if err != nil {
+		return initialGame()
+	}
+
+	var model model
+	err = json.Unmarshal(jsonData, &model)
+	if err != nil {
+		panic("Error unmarshalling JSON: " + err.Error())
+	}
+
+	// if the game gets upgraded, reset the local game
+	// TODO migrate data lol
+	if model.GameVersion != GAME_VERSION {
+		return initialGame()
+	}
+
+	return model
+}
+
+func (m *model) distributeTokens() {
+	now := time.Now()
+	if m.LastPlayed.Year() < now.Year() ||
+		m.LastPlayed.Month() < now.Month() ||
+		m.LastPlayed.Day() < now.Day() {
+		m.Tokens += DAILY_TOKENS
+		m.SpinnerMsg = "It's a new day! Have " + strconv.Itoa(
+			DAILY_TOKENS,
+		) + " tokens on the house :)"
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	m.distributeTokens()
+	if m.Spinning {
+		return m.doTick()
+	}
+	return nil
+}
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
@@ -355,31 +412,33 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 
 		case "ctrl+c", "q":
+			m.saveGame()
 			return m, tea.Quit
 
 		case " ":
-			if m.spinning {
-				m.spinnerMsg = "Chill, the spinner is still spinning!"
+			if m.Spinning {
+				m.SpinnerMsg = "Chill, the spinner is still spinning!"
 				return m, nil
-			} else if m.tokens == 0 && !m.isFreeSpin {
-				m.spinnerMsg = "You have no more tokens!"
+			} else if m.Tokens == 0 && !m.IsFreeSpin {
+				m.SpinnerMsg = "You have no more tokens!"
 				return m, nil
-			} else if m.tokens < 0 {
-				m.spinnerMsg = "How the heck do you have negative tokens?"
+			} else if m.Tokens < 0 {
+				m.SpinnerMsg = "How the heck do you have negative tokens?"
 				return m, nil
 			}
 
 			// free spin logic
-			if m.isFreeSpin {
-				m.spinnerMsg = "Enjoy the free spin!"
-				m.isFreeSpin = false
+			if m.IsFreeSpin {
+				m.SpinnerMsg = "Enjoy the free spin!"
+				m.IsFreeSpin = false
 			} else {
-				m.spinnerMsg = ""
-				m.tokens--
+				m.SpinnerMsg = ""
+				m.Tokens--
 			}
-
-			m.ticksLeft = 90
-			m.spinning = true
+			m.TicksLeft = SPIN_TICKS
+			m.Spinning = true
+			m.updateLastPlayed()
+			m.saveGame()
 			return m, m.doTick()
 		}
 	}
@@ -389,24 +448,24 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m model) View() string {
 	slotStr := "         " + lipgloss.NewStyle().
-		Render(m.slot.toStringArray()...) +
-		"  x" + strconv.Itoa(m.multiplier) + " mult"
+		Render(m.Slot.toStringArray()...) +
+		"  x" + strconv.Itoa(m.Multiplier) + " mult"
 
-	tokenStr := strconv.Itoa(m.tokens)
-	dollarsStr := strconv.Itoa(m.dollars)
-	jackpotAmountStr := strconv.Itoa(m.jackpotAmount)
+	tokenStr := strconv.Itoa(m.Tokens)
+	dollarsStr := strconv.Itoa(m.Dollars)
+	jackpotAmountStr := strconv.Itoa(m.JackpotAmount)
 
 	feverModeStr := ""
-	if m.feverMode {
+	if m.FeverMode {
 		feverModeStr = "\n" + emoji.Fire.String() + "You're on fire! Your next win will be doubled!" + emoji.Fire.String()
 	}
 
 	timeStr := "It is currently daytime! You are safe " + emoji.Sunrise.String()
-	if !m.isDay {
+	if !m.IsDay {
 		timeStr = "It is currently nighttime! Watch out for the Wheel of Misfortune! " + emoji.Skull.String()
 	}
 
-	consoleTxt := "\n\n" + m.spinnerMsg +
+	consoleTxt := "\n\n" + m.SpinnerMsg +
 		"\nPress the spacebar to spin!" +
 		"\nYou have " + tokenStr + " tokens left" +
 		"\nYou have " + dollarsStr + " dollars" +
@@ -436,7 +495,7 @@ func (m model) View() string {
 }
 
 func main() {
-	p := tea.NewProgram(initialModel(), tea.WithFPS(120), tea.WithAltScreen())
+	p := tea.NewProgram(loadGame(), tea.WithFPS(120), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
